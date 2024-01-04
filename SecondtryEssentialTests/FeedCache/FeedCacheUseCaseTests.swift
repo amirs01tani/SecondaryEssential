95489161d265354bc58039084db3528d77a1c638
amir soltani <amirsoltani686@gmail.com>
2024-01-04 20:50:31 +0330

    Succeeds on successful cache insertion

--- SecondtryEssentialTests/FeedCache/FeedCacheUseCaseTests.swift
+++ SecondtryEssentialTests/FeedCache/FeedCacheUseCaseTests.swift
@@ -54,10 +54,15 @@ class FeedStore {
     func completeInsertion(with error: Error, at index: Int = 0) {
         insertCompletions[index](error)
     }
+    
+    func completeInsertionSuccessfully(at index: Int = 0) {
+        insertCompletions[index](nil)
+    }
+    
 }
 
 final class FeedCacheUseCaseTests: XCTestCase {
-
+    
     func test_init_doesNotDeleteCacheUnderCreation() {
         let (_, store) = makeSUT()
         XCTAssertEqual(store.deletionCompletions.count, 0)
@@ -131,6 +136,23 @@ final class FeedCacheUseCaseTests: XCTestCase {
         store.completeInsertion(with: insertionError)
         wait(for: [exp], timeout: 1.0)
         XCTAssertEqual(receivedError as NSError?, insertionError)
+    }
+    
+    func test_save_succeedsOnSuccessfulCacheInsertion() {
+        let items = [uniqueItem, uniqueItem]
+        let (sut, store) = makeSUT()
+        let exp = expectation(description:
+                                "Wait for save completion")
+        var receivedError: Error?
+        
+        sut.save(items) { error in
+            receivedError = error
+            exp.fulfill()
+        }
+        store.completeDeletionSuccessfully()
+        store.completeInsertionSuccessfully()
+        wait(for: [exp], timeout: 1.0)
+        XCTAssertNil(receivedError)
     }
 }
 
