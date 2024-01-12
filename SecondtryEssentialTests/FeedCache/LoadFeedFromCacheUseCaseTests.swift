8ee06d979e977760ac47d66616c8be82d78510dd
amir soltani <amirsoltani686@gmail.com>
2024-01-12 23:58:27 +0330

    Loads doesNotdeleteCacheOnLessThanSevenDaysOldCache

--- SecondtryEssentialTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
+++ SecondtryEssentialTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
@@ -76,11 +76,22 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
     }
     
-    func test_load_DoesNotdeleteCacheOnEmptyCache() {
+    func test_load_doesNotdeleteCacheOnEmptyCache() {
         let (sut, store) = makeSUT()
         sut.load { _ in }
         store.completeRetrievalSuccessfully()
         XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
+    func test_load_doesNotdeleteCacheOnLessThanSevenDaysOldCache() {
+        let feed = uniqueItems()
+        let fixedCurrentDate = Date()
+        let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding (days: -7).adding(seconds: 1)
+        let (sut, store) = makeSUT(currentDate: fixedCurrentDate )
+        sut.load { _ in }
+        store.completeRetrieval(with: feed.local, timeStamp: lessThanSevenDaysOldTimestamp)
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+        
     }
     
     // MARK: - Helpers
