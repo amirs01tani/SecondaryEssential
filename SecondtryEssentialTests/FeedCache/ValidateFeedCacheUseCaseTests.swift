a73914893db8745f6996aefdc6920358817add3d
amir soltani <amirsoltani686@gmail.com>
2024-01-16 23:43:30 +0330

    ValidateCache doesNotDeleteInvalidCacheAfterSUTInstanceHasBeenDeallocated

--- SecondtryEssentialTests/FeedCache/ValidateFeedCacheUseCaseTests.swift
+++ SecondtryEssentialTests/FeedCache/ValidateFeedCacheUseCaseTests.swift
@@ -33,6 +33,15 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
         
     }
     
+    func test_validateCache_doesNotDeleteInvalidCacheAfterSUTInstanceHasBeenDeallocated() {
+        let store = FeedStoreSpy()
+        var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
+        sut?.validateCache()
+        sut = nil
+        store.completeRetrieval (with: anyNSError())
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(currentDate: Date = Date(), file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
