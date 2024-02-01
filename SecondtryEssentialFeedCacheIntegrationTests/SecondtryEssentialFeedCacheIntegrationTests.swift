a8d3fed26d16b86ca143c096f93da83c7f012248
amir soltani <amirsoltani686@gmail.com>
2024-02-01 22:27:08 +0330

    adds setup ond tear down method to the feed cache integration tests

--- SecondtryEssentialFeedCacheIntegrationTests/SecondtryEssentialFeedCacheIntegrationTests.swift
+++ SecondtryEssentialFeedCacheIntegrationTests/SecondtryEssentialFeedCacheIntegrationTests.swift
@@ -10,6 +10,18 @@ import SecondtryEssential
 
 final class SecondtryEssentialFeedCacheIntegrationTests: XCTestCase {
     
+    override func setUp() {
+        super.setUp()
+        
+        setupEmptyStoreState()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        undoStoreSideEffects()
+    }
+    
     func test_load_deliversNoItemsOnEmptyCache() {
         let sut = makeSUT()
         
@@ -39,6 +51,19 @@ final class SecondtryEssentialFeedCacheIntegrationTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
+    
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+    
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+    
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+    }
+    
     
     private func testSpecificStoreURL() -> URL {
         return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
