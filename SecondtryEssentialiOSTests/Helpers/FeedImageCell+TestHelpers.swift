47ad383fb6fda8ac245d70739d6486e45cee8b44
amir soltani <amirsoltani686@gmail.com>
2024-02-11 19:00:46 +0330

    Moves FeedImageCell to its own file

---
+++ SecondtryEssentialiOSTests/Helpers/FeedImageCell+TestHelpers.swift
@@ -0,0 +1,40 @@
+//
+//  FeedImageCell+TestHelpers.swift
+//  SecondtryEssentialiOSTests
+//
+//  Created by Amir on 2/11/24.
+//
+
+import Foundation
+import UIKit
+import SecondtryEssentialiOS
+
+extension FeedImageCell {
+    var isShowingLocation: Bool {
+        return !locationContainer.isHidden
+    }
+
+    var locationText: String? {
+        return locationLabel.text
+    }
+
+    var descriptionText: String? {
+        return descriptionLabel.text
+    }
+    
+    var isShowingImageLoadingIndicator: Bool {
+        return feedImageContainer.isShimmering
+    }
+    
+    var renderedImage: Data? {
+        return feedImageView.image?.pngData()
+    }
+    
+    var isShowingRetryAction: Bool {
+        return !feedImageRetryButton.isHidden
+    }
+    
+    func simulateRetryAction() {
+        feedImageRetryButton.simulateTap()
+    }
+}
