98db05c5658dbb091597694f30dfa03b8db1bcd5
amir soltani <amirsoltani686@gmail.com>
2024-02-11 12:32:37 +0330

    Moves FeedViewController to its own class

---
+++ SecondtryEssentialiOS/FeedViewController.swift
@@ -0,0 +1,54 @@
+//
+//  FeedViewController.swift
+//  SecondtryEssentialiOS
+//
+//  Created by Amir on 2/11/24.
+//
+
+import Foundation
+import UIKit
+import SecondtryEssential
+
+public class FeedViewController: UITableViewController {
+    private var loader: FeedLoader?
+    private var tableModel = [FeedImage]()
+    
+    public convenience init(loader: FeedLoader) {
+        self.init()
+        self.loader = loader
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+    }
+    
+    public override func viewIsAppearing(_ animated: Bool) {
+        super.viewIsAppearing(animated)
+        
+        load()
+    }
+    
+    @objc private func load() {
+        refreshControl?.beginRefreshing()
+        loader?.load { [weak self] result in
+            self?.tableModel = (try? result.get()) ?? []
+            self?.tableView.reloadData()
+            self?.refreshControl?.endRefreshing()
+        }
+    }
+    
+    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableModel.count
+    }
+    
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellModel = tableModel[indexPath.row]
+        let cell = FeedImageCell()
+        cell.locationContainer.isHidden = (cellModel.location == nil)
+        cell.locationLabel.text = cellModel.location
+        cell.descriptionLabel.text = cellModel.description
+        return cell
+    }
+}
--- SecondtryEssentialiOSTests/SecondtryEssentialiOSTests.swift
+++ SecondtryEssentialiOSTests/SecondtryEssentialiOSTests.swift
@@ -9,50 +9,6 @@ import XCTest
 import SecondtryEssential
 import SecondtryEssentialiOS
 
-class FeedViewController: UITableViewController {
-    private var loader: FeedLoader?
-    private var tableModel = [FeedImage]()
-    
-    convenience init(loader: FeedLoader) {
-        self.init()
-        self.loader = loader
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        refreshControl = UIRefreshControl()
-        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
-    }
-    
-    override func viewIsAppearing(_ animated: Bool) {
-        super.viewIsAppearing(animated)
-        
-        load()
-    }
-    
-    @objc private func load() {
-        refreshControl?.beginRefreshing()
-        loader?.load { [weak self] result in
-            self?.tableModel = (try? result.get()) ?? []
-            self?.tableView.reloadData()
-            self?.refreshControl?.endRefreshing()
-        }
-    }
-    
-    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tableModel.count
-    }
-    
-    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellModel = tableModel[indexPath.row]
-        let cell = FeedImageCell()
-        cell.locationContainer.isHidden = (cellModel.location == nil)
-        cell.locationLabel.text = cellModel.location
-        cell.descriptionLabel.text = cellModel.description
-        return cell
-    }
-}
-
 final class FeedViewControllerTests: XCTestCase {
     
     func test_init_doesNotLoadFeed() {
@@ -228,7 +184,19 @@ extension UIRefreshControl {
     }
 }
 
-extension FeedViewController {
+private class FakeRefreshControl: UIRefreshControl {
+    private var _isRefreshing = false
+    override var isRefreshing: Bool {
+        _isRefreshing }
+    override func beginRefreshing() {
+        _isRefreshing = true
+    }
+    override func endRefreshing() {
+        _isRefreshing = false
+    }
+}
+
+private extension FeedViewController {
     func simulateAppearance() {
         if !isViewLoaded {
             loadViewIfNeeded()
@@ -261,21 +229,7 @@ extension FeedViewController {
     private var feedImagesSection: Int {
         return 0
     }
-}
-
-private class FakeRefreshControl: UIRefreshControl {
-    private var _isRefreshing = false
-    override var isRefreshing: Bool {
-        _isRefreshing }
-    override func beginRefreshing() {
-        _isRefreshing = true
-    }
-    override func endRefreshing() {
-        _isRefreshing = false
-    }
-}
-
-private extension FeedViewController {
+    
     func simulateUserInitiatedFeedReload() {
         replaceRefreshControlWithFakeForiOS17Support()
         refreshControl?.simulatePullToRefresh()
