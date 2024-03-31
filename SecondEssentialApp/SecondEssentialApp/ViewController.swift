18431c87d1fa5c6cee665e2beffffd12cf276739
amir soltani <amirsoltani686@gmail.com>
2024-03-31 17:52:08 +0330

    Adds empty Project

---
+++ SecondEssentialApp/SecondEssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondEssentialApp
+//
+//  Created by Amir on 3/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
