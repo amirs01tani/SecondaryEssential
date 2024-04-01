7650b71272cd517e4e0ca916e09612a23c0cd2b6
amir soltani <amirsoltani686@gmail.com>
2024-04-01 21:03:45 +0330

    Loads image from primary first

--- SecondEssentialApp/SecondEssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
+++ SecondEssentialApp/SecondEssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -9,6 +9,7 @@ import XCTest
 import SecondtryEssential
 
 class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
+    private let primary: FeedImageDataLoader
     private class Task: FeedImageDataLoaderTask {
         func cancel() {
 
@@ -16,10 +17,11 @@ class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
     }
 
     init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {
-
+        self.primary = primary
     }
 
     func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        _ = primary.loadImageData(from: url) { _ in }
         return Task()
     }
 }
@@ -32,6 +34,18 @@ class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
         _ = FeedImageDataLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
 
         XCTAssertTrue(primaryLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the primary loader")
+        XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
+    }
+    
+    func test_loadImageData_loadsFromPrimaryLoaderFirst() {
+        let url = anyURL()
+        let primaryLoader = LoaderSpy()
+        let fallbackLoader = LoaderSpy()
+        let sut = FeedImageDataLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+        
+        _ = sut.loadImageData(from: url) { _ in }
+        
+        XCTAssertEqual(primaryLoader.loadedURLs, [url], "Expected to load URL from primary loader")
         XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
     }
 
@@ -52,6 +66,10 @@ class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
             messages.append((url, completion))
             return Task()
         }
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://a-url.com")!
     }
 
 }
