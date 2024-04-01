e19b1090268be7c9b9b6a59f6d54d96077f24c5c
amir soltani <amirsoltani686@gmail.com>
2024-04-01 20:41:34 +0330

    Delivers fallback when primary is Fucked up

--- SecondEssentialApp/SecondEssentialAppTests/FeedLoaderWithFallBackCompositeTests.swift
+++ SecondEssentialApp/SecondEssentialAppTests/FeedLoaderWithFallBackCompositeTests.swift
@@ -10,13 +10,22 @@ import SecondtryEssential
 
 class FeedLoaderWithFallbackComposite: FeedLoader {
     private let primary: FeedLoader
+    private let fallback: FeedLoader
 
     init(primary: FeedLoader, fallback: FeedLoader) {
         self.primary = primary
+        self.fallback = fallback
     }
 
     func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        primary.load(completion: completion)
+        primary.load() { [weak self] result in
+            switch result {
+            case .success(let images):
+                completion(.success(images))
+            case .failure( _):
+                self?.fallback.load(completion: completion)
+            }
+        }
     }
 }
 
@@ -41,6 +50,27 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
         }
         wait(for: [exp], timeout: 1)
     }
+    
+    func test_load_deliversFallbackFeedOnPrimaryLoaderFailure() {
+            let fallbackFeed = uniqueFeed()
+            let sut = makeSUT(primaryResult: .failure(anyNSError()), fallbackResult: .success(fallbackFeed))
+
+            let exp = expectation(description: "Wait for load completion")
+
+            sut.load { result in
+                switch result {
+                case let .success(receivedFeed):
+                    XCTAssertEqual(receivedFeed, fallbackFeed)
+
+                case .failure:
+                    XCTFail("Expected successful load feed result, got \(result) instead")
+                }
+
+                exp.fulfill()
+            }
+
+            wait(for: [exp], timeout: 1.0)
+        }
     
     private func uniqueFeed() -> [FeedImage] {
         return [FeedImage(id: UUID(), description: "any", location: "any", imageURL: URL(string: "http://any-url.com")!)]
@@ -72,6 +102,10 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
         addTeardownBlock { [weak instance] in
             XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
         }
+    }
+    
+    private func anyNSError() -> NSError {
+        return NSError(domain: "any error", code: 0)
     }
     
 }
