d6f4ac31674316dd8c0b19ee1d2dc5592485b159
amir soltani <amirsoltani686@gmail.com>
2024-04-02 11:50:17 +0330

    Extract FeedImageDataLoader test helpers into a shared protocol

---
+++ SecondEssentialApp/SecondEssentialAppTests/Shared/XCTestCase+FeedImageDataLoader.swift
@@ -0,0 +1,37 @@
+//
+//  XCTestCase+FeedImageDataLoader.swift
+//  SecondEssentialAppTests
+//
+//  Created by Amir on 4/2/24.
+//
+
+import Foundation
+import XCTest
+import SecondtryEssential
+
+protocol FeedImageDataLoaderTests: XCTestCase {}
+
+extension FeedImageDataLoaderTests {
+    func expect(_ sut: FeedImageDataLoader, toCompleteWith expectedResult: FeedImageDataLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "Wait for load completion")
+        
+        _ = sut.loadImageData(from: anyURL()) { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedFeed), .success(expectedFeed)):
+                XCTAssertEqual(receivedFeed, expectedFeed, file: file, line: line)
+                
+            case (.failure, .failure):
+                break
+                
+            default:
+                XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+            }
+            
+            exp.fulfill()
+        }
+        
+        action()
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+}
