1ccb8746effadf6cbdadab6647a013cade0f85e5
amir soltani <amirsoltani686@gmail.com>
2024-04-10 21:33:49 +0330

    Displays remote feed on launch when customer has connectivity

--- SecondEssentialApp/SecondEssentialApp/SceneDelegate.swift
+++ SecondEssentialApp/SecondEssentialApp/SceneDelegate.swift
@@ -11,36 +11,23 @@ import SecondtryEssentialiOS
 import CoreData
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
-        let remoteURL = URL(string:
-        "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+        let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
         
         let remoteClient = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
-        let remoteFeedLoader = RemoteFeedLoader (url: remoteURL, client:
-        remoteClient)
-        let remoteImageLoader = RemoteFeedImageDataLoader (client: remoteClient)
-        let localStoreURL = NSPersistentContainer.defaultDirectoryURL()
-            .appendingPathComponent("feed-store.sqlite")
-        let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
-        let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
-        let localImageLoader = LocalFeedImageDataLoader(store: localStore)
-        window?.rootViewController = MVPFeedUIComposer.feedComposedWith(
-            feedLoader:
-                FeedLoaderWithFallbackComposite(
-                primary: FeedLoaderCacheDecorator(decoratee: remoteFeedLoader, cache: localFeedLoader),
-                fallback: localFeedLoader),
-            imageLoader:
-                FeedImageDataLoaderWithFallbackComposite(
-                    primary: FeedImageDataLoaderCacheDecorator(decoratee: remoteImageLoader, cache: localImageLoader),
-                    fallback: localImageLoader))
+        let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
+        let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
         
+        window?.rootViewController = MVPFeedUIComposer.feedComposedWith(
+            feedLoader: remoteFeedLoader,
+            imageLoader: remoteImageLoader)
         
     }
-
+    
 }
 
--- SecondEssentialApp/SecondEssentialAppUITests/SecondEssentialAppUITests.swift
+++ SecondEssentialApp/SecondEssentialAppUITests/SecondEssentialAppUITests.swift
@@ -8,5 +8,12 @@
 import XCTest
 
 final class SecondEssentialAppUITests: XCTestCase {
-
+    func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
+        let app = XCUIApplication()
+        
+        app.launch()
+        
+        XCTAssertEqual(app.cells.count, 22)
+        XCTAssertEqual(app.cells.firstMatch.images.count, 1)
+    }
 }
