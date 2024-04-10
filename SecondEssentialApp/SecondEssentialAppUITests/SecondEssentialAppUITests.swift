e3c6446ec3d2f7f28c444210d2925b9113dba29f
amir soltani <amirsoltani686@gmail.com>
2024-04-10 21:51:12 +0330

    Add #if DEBUG compilation directive

--- SecondEssentialApp/SecondEssentialAppUITests/SecondEssentialAppUITests.swift
+++ SecondEssentialApp/SecondEssentialAppUITests/SecondEssentialAppUITests.swift
@@ -10,7 +10,7 @@ import XCTest
 final class SecondEssentialAppUITests: XCTestCase {
     func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
         let app = XCUIApplication()
-        
+        app.launchArguments = ["-reset"]
         app.launch()
     
         let feedCells = app.cells.matching(identifier: "feed-image-cell")
@@ -23,6 +23,7 @@ final class SecondEssentialAppUITests: XCTestCase {
     
     func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
             let onlineApp = XCUIApplication()
+            onlineApp.launchArguments = ["-reset"]
             onlineApp.launch()
 
             let offlineApp = XCUIApplication()
